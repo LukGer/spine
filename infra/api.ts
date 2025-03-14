1be5ea3dc0487940b2f982cff9eacb3846d5c0f2
Lukas Gerhold <lukger1999@gmail.com>
2025-03-14 17:41:25 +0100

    key

--- infra/api.ts
+++ infra/api.ts
@@ -5,7 +5,7 @@ export const api = new sst.cloudflare.Worker("apiWorker", {
   handler: "./apps/api/src/index.ts",
   environment: {
     LOG_LEVEL: "info",
-    GOOGLE_API_KEY: "AIzaSyB4Lop47yW2Y7dOkqvHY_MLeZeejbGEOdw",
+    GOOGLE_API_KEY: "***",
   },
   domain,
 });
